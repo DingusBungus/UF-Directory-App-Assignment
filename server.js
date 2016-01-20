a0d57104ad89af2d913e41343d7f9a21468784f0
Conor Doherty <conor.doherty@ufl.edu>
2016-01-20 00:39:04 -0500

    Adding Assignment 1
    Just adding the code for Assignment 1 so all of it is in the same place

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+  /* Check the parsed url for /listings and display listings if found */
+   if (parsedUrl.path == '/listings') {
+      response.end(listingData);
+   } else { /* Otherwise set status to 404 and display Bad Gateway Error */
+      response.statusCode = 404;
+      response.end('Bad gateway error');
+   }
 };
 
+/* toString() the file for printing */
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+   listingData = data.toString();
+
+   /* Actually create the server*/
+  server = http.createServer(requestHandler);
+  server.listen(port, function() {
+      console.log('Listening...');
+  });
 });
